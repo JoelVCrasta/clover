c144b04ab8c02def8f41be28bbf52f592dc17340
JoelVCrasta <joelcrasta03@proton.me>
2025-05-24 20:33:17 +0530

    bitfield TODO: add has and set for pieces

---
+++ client/message.go
@@ -0,0 +1,45 @@
+package client
+
+// <lengthPrefix : 4 bytes> <messageId : 1 byte> <payload : variable size>
+type MessageId int
+
+const (
+	ChokeId MessageId = iota
+	UnchokeId
+	InterestedId
+	NotInterestedId
+	HaveId
+	BitfieldId
+	RequestId
+	PieceId
+	CancelId
+	PortId
+)
+
+// lengthPrefix is the length of the message prefix for each message type
+var lengthPrefix = map[MessageId]int{
+	ChokeId:         1,
+	UnchokeId:       1,
+	InterestedId:    1,
+	NotInterestedId: 1,
+	HaveId:          5,
+	RequestId:       13,
+	CancelId:        13,
+	PortId:          3,
+}
+
+// payloadSize is the size of the payload for each message type
+var payloadSize = map[MessageId]int{
+	ChokeId:         0,
+	UnchokeId:       0,
+	InterestedId:    0,
+	NotInterestedId: 0,
+	HaveId:          4,
+	RequestId:       12,
+	CancelId:        12,
+	PortId:          2,
+}
+
+// KeepAlive is to send to peer to keep the connection alive
+var KeepAlive = []byte{0, 0, 0, 0}
+ 
