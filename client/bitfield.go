6171b650fa1cf90344d6d413bf0ea59283c439e5
JoelVCrasta <joelcrasta03@proton.me>
2025-05-26 17:28:25 +0530

    :added piece has and set check function

--- client/bitfield.go
+++ client/bitfield.go
@@ -1,3 +1,25 @@
 package client
 
 type Bitfield []byte
+
+// Has checks if the bit at the given index in the bitfield is set to 1.
+func (b Bitfield) Has(index int) bool {
+	byteIndex := index / 8
+	if byteIndex < 0 || byteIndex >= len(b) {
+		return false
+	}
+
+	bitIndex := index % 8
+	return b[byteIndex]>>(7-bitIndex)&1 == 1
+}
+
+// Set sets the bit at the given index in the bitfield to 1.
+func (b Bitfield) Set(index int) {
+	byteIndex := index / 8
+	if byteIndex < 0 || byteIndex >= len(b) {
+		return
+	}
+
+	bitIndex := index % 8
+	b[byteIndex] |= (1 << (7 - bitIndex))
+}
