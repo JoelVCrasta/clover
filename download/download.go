92934ad9bc406c844fdff8041b415ac044bf38df
JoelVCrasta <joelcrasta03@proton.me>
2025-06-23 15:04:00 +0530

    added piece download from peers using goroutines

--- download/download.go
+++ download/download.go
@@ -26,7 +26,7 @@ type DownloadManager struct {
 	Mutex sync.Mutex
 }
 
-type NewPiece struct {
+type workPiece struct {
 	index  int
 	buf    []byte
 	hash   [20]byte
@@ -35,6 +35,11 @@ type NewPiece struct {
 	downloadedBytes int
 
 	peer *client.ActivePeer
+}
+
+type completedPiece struct {
+	index int
+	buf   []byte
 }
 
 func NewDownloadManager(c *client.Client) *DownloadManager {
@@ -46,34 +51,116 @@ func NewDownloadManager(c *client.Client) *DownloadManager {
 	}
 }
 
+func NewWorkPiece(index int, peer *client.ActivePeer, hash [20]byte, length int) *workPiece {
+	return &workPiece{
+		index:           index,
+		buf:             make([]byte, length),
+		hash:            hash,
+		length:          length,
+		peer:            peer,
+		downloadedBytes: 0,
+	}
+}
+
+func (dm *DownloadManager) Download() {
+	// Fill the piece queue with piece indices
+	for i := range dm.Client.Torrent.PiecesHash {
+		dm.PieceQueue <- i
+	}
+
+	completedPieces := make(chan *completedPiece)
+
+	// Start downloading pieces from peers
+	for _, peer := range dm.Client.ActivePeers {
+		go dm.peerDownload(peer, completedPieces)
+	}
+
+}
+
+func (dm *DownloadManager) peerDownload(peer *client.ActivePeer, completedPieces chan *completedPiece) {
+	log.Printf("Starting download from peer %s:%d", peer.IpAddr, peer.Port)
+
+	_ = peer.SendInterested()
+
+	// Process pieces from the piece queue
+	for work := range dm.PieceQueue {
+		if !peer.Bitfield.Has(work) {
+			dm.PieceQueue <- work // Requeue if peer does not have the piece
+			continue
+		}
+
+		if dm.Downloaded[work] {
+			continue // Skip already downloaded pieces
+		}
+
+		dm.Mutex.Lock()
+		if dm.Requested[work] {
+			dm.Mutex.Unlock()
+			continue // Skip already requested pieces
+		}
+		dm.Requested[work] = true
+		dm.Mutex.Unlock()
+
+		log.Printf("Downloading piece %d from peer %s:%d", work, peer.IpAddr, peer.Port)
+
+		// Calculate the length of the piece and create a WorkPiece for piece download
+		length := dm.calculatePieceLength(work)
+		piece := NewWorkPiece(work, peer, dm.Client.Torrent.PiecesHash[work], length)
+
+		err := piece.downloadPiece(&dm.Mutex)
+		if err != nil {
+			log.Print(err.Error())
+			dm.PieceQueue <- work // Requeue the piece if download failed
+			continue
+		}
+
+		if !piece.verify() {
+			log.Printf("Piece %d from peer %s:%d failed hash verification", work, peer.IpAddr, peer.Port)
+			dm.PieceQueue <- work // Requeue the piece if verification failed
+			continue
+		}
+
+		log.Printf("Downloaded piece %d from peer %s:%d", work, peer.IpAddr, peer.Port)
+		dm.Mutex.Lock()
+		dm.Downloaded[work] = true // Mark the piece as downloaded
+		dm.Requested[work] = false // Mark the piece as no longer requested
+		dm.Mutex.Unlock()
+
+		// Send the completed piece to the channel
+		completedPieces <- &completedPiece{
+			index: work,
+			buf:   piece.buf,
+		}
+	}
+}
 
 /*
 downloadPiece downloads a piece from a peer.
 It sends requests to the peer until the entire piece is downloaded.
 If the peer is choked, it will wait until the peer unchokes before continuing.
 */
-func (np *NewPiece) downloadPiece(peer *client.ActivePeer, mu *sync.Mutex) error {
-	_ = peer.Conn.SetDeadline(time.Now().Add(config.Config.PieceMessageTimeout))
-	defer peer.Conn.SetDeadline(time.Time{})
+func (wp *workPiece) downloadPiece(mu *sync.Mutex) error {
+	_ = wp.peer.Conn.SetDeadline(time.Now().Add(config.Config.PieceMessageTimeout))
+	defer wp.peer.Conn.SetDeadline(time.Time{})
 
-	for np.downloadedBytes < np.length {
+	for wp.downloadedBytes < wp.length {
 		// Sleep if the peer is choked
-		if peer.Choked {
+		if wp.peer.Choked {
 			time.Sleep(100 * time.Millisecond)
 			continue
 		}
 
 		// Calculate the size of the next request
-		remainingBytes := np.length - np.downloadedBytes
+		remainingBytes := wp.length - wp.downloadedBytes
 		requestSize := min(remainingBytes, MAX_BLOCK_SIZE)
 
-		err := peer.SendRequest(np.index, np.downloadedBytes, requestSize)
+		err := wp.peer.SendRequest(wp.index, wp.downloadedBytes, requestSize)
 		if err != nil {
 			return err
 		}
 
 		// Read the piece data from the peer
-		err = np.read(mu)
+		err = wp.read(mu)
 		if err != nil {
 			return err
 		}
@@ -99,9 +186,9 @@ func (dm *DownloadManager) calculatePieceLength(index int) int {
 }
 
 // verify checks if the piece's sha1 hash matches the expected hash.
-func (np *NewPiece) verify() bool {
-	pieceHash := sha1.Sum(np.buf)
-	return bytes.Equal(pieceHash[:], np.hash[:])
+func (wp *workPiece) verify() bool {
+	pieceHash := sha1.Sum(wp.buf)
+	return bytes.Equal(pieceHash[:], wp.hash[:])
 }
 
 /*
@@ -109,12 +196,12 @@ read reads messages from the peer connection.
 It processes different message types such as Choke, Unchoke, Have, Bitfield, Piece, and Port (for now only leeching)
 It updates the peer's state based on the received messages.
 */
-func (np *NewPiece) read(mu *sync.Mutex) error {
-	msg, err := message.ReadMessage(np.peer.Conn)
+func (wp *workPiece) read(mu *sync.Mutex) error {
+	msg, err := message.ReadMessage(wp.peer.Conn)
 	if err != nil {
 		// Peer connection closed
 		if err == io.EOF {
-			return fmt.Errorf("connection closed by peer %s:%d", np.peer.IpAddr, np.peer.Port)
+			return fmt.Errorf("connection closed by peer %s:%d", wp.peer.IpAddr, wp.peer.Port)
 		}
 
 		return err
@@ -130,38 +217,38 @@ func (np *NewPiece) read(mu *sync.Mutex) error {
 
 	switch msg.MessageId {
 	case message.ChokeId:
-		np.peer.Choked = true
+		wp.peer.Choked = true
 
 	case message.UnchokeId:
-		np.peer.Choked = false
+		wp.peer.Choked = false
 
 	case message.HaveId:
 		pieceIndex, err := msg.DecodeHave()
 		if err != nil {
 			return err
 		}
-		np.peer.Bitfield.Set(pieceIndex)
+		wp.peer.Bitfield.Set(pieceIndex)
 
 	case message.BitfieldId:
 		bitfield, err := msg.DecodeBitfield()
 		if err != nil {
 			return err
 		}
-		np.peer.Bitfield = bitfield
+		wp.peer.Bitfield = bitfield
 
 	case message.PieceId:
-		offset, block, err := msg.DecodePiece(np.index, np.length)
+		offset, block, err := msg.DecodePiece(wp.index, wp.length)
 		if err != nil {
 			return err
 		}
-		copy(np.buf[offset:], block)
-		np.downloadedBytes += len(block)
+		copy(wp.buf[offset:], block)
+		wp.downloadedBytes += len(block)
 
 	case message.PortId:
-		log.Printf("Peer %s:%d sent Port message with port %x", np.peer.IpAddr, np.peer.Port, msg.Payload)
+		log.Printf("Peer %s:%d sent Port message with port %x", wp.peer.IpAddr, wp.peer.Port, msg.Payload)
 
 	default:
-		return fmt.Errorf("unknown message type %d from peer %s:%d", msg.MessageId, np.peer.IpAddr, np.peer.Port)
+		return fmt.Errorf("unknown message type %d from peer %s:%d", msg.MessageId, wp.peer.IpAddr, wp.peer.Port)
 	}
 
 	return nil
