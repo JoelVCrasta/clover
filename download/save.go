11ab6c3910e441ed3dcb325412f2572922c52e7d
JoelVCrasta <joelcrasta03@proton.me>
2025-06-25 16:21:27 +0530

    save file to disk (not implemented yet)

---
+++ download/save.go
@@ -0,0 +1,42 @@
+package download
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/JoelVCrasta/config"
+	"github.com/JoelVCrasta/parsing"
+)
+
+type FileRegion struct {
+	Path   string
+	Length int
+	Start  int
+	End    int
+	File   *os.File
+}
+
+func createSaveFile(torrent parsing.Torrent) (*os.File, error) {
+	downloadDir := config.Config.DownloadDirectory
+
+	// Multifile torrent
+	if torrent.IsMultiFile {
+		path := path.Join(downloadDir, torrent.Info.Name)
+		err := os.MkdirAll(path, 0755)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create download directory: %w", err)
+		}
+
+		return nil, nil
+	}
+
+	// Single file torrent
+	path := path.Join(downloadDir, torrent.Info.Name)
+	file, err := os.Create(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create save file: %w", err)
+	}
+
+	return file, nil
+}
