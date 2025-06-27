832c3a8a99944a1bbcf934cb21cb6cc80be0e662
JoelVCrasta <joelcrasta03@proton.me>
2025-06-27 15:51:07 +0530

    updated the config go file

--- config/config.go
+++ config/config.go
@@ -3,22 +3,24 @@ package config
 import "time"
 
 type GlobalConfig struct {
-	MinPeers              int
-	Port                  uint16
-	TrackerConnectTimeout time.Duration
-	PeerHandshakeTimeout  time.Duration
-	PieceMessageTimeout   time.Duration
-	DownloadDirectory     string
-	MaxConnections        int
-	PeerId                [20]byte
+	MinPeers               int
+	Port                   uint16
+	TrackerConnectTimeout  time.Duration
+	PeerHandshakeTimeout   time.Duration
+	PieceMessageTimeout    time.Duration
+	DefaultTrackerInterval time.Duration
+	DownloadDirectory      string
+	MaxConnections         int
+	PeerId                 [20]byte
 }
 
 var Config = GlobalConfig{
-	MinPeers:              10,
-	Port:                  6881,
-	TrackerConnectTimeout: 5 * time.Second,
-	PeerHandshakeTimeout:  3 * time.Second,
-	PieceMessageTimeout:   25 * time.Second,
-	DownloadDirectory:     "./Downloads",
-	MaxConnections:        100,
+	MinPeers:               10,
+	Port:                   6881,
+	TrackerConnectTimeout:  5 * time.Second,
+	PeerHandshakeTimeout:   3 * time.Second,
+	PieceMessageTimeout:    25 * time.Second,
+	DefaultTrackerInterval: 30 * time.Second,
+	DownloadDirectory:      "./Downloads",
+	MaxConnections:         50,
 }
