13aa039ae3ff7d06bc00060e6b827b9bafd20cb1
JoelVCrasta <loejstarc@gmail.com>
2025-10-16 19:55:20 +0530

    updated and fixed download dir path issue

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "time"
+import (
+	"os"
+	"path/filepath"
+	"time"
+)
 
 type GlobalConfig struct {
 	MinPeers               int
@@ -15,14 +19,25 @@ type GlobalConfig struct {
 	PeerId                 [20]byte
 }
 
-var Config = GlobalConfig{
-	MinPeers:               10,
-	Port:                   6881,
-	TrackerConnectTimeout:  5 * time.Second,
-	PeerHandshakeTimeout:   20 * time.Second,
-	PieceMessageTimeout:    30 * time.Second,
-	DefaultTrackerInterval: 1800, // 20 minutes
-	DownloadDirectory:      "./Downloads",
-	MaxTrackerConnections:  20,
-	MaxFailedRetries:       3,
+var Config GlobalConfig
+
+func init() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		home = "."
+	}
+
+	defaultDownloadDir := filepath.Join(home, "Downloads")
+
+	Config = GlobalConfig{
+		MinPeers:               10,
+		Port:                   6881,
+		TrackerConnectTimeout:  5 * time.Second,
+		PeerHandshakeTimeout:   20 * time.Second,
+		PieceMessageTimeout:    30 * time.Second,
+		DefaultTrackerInterval: 1800, // 20 minutes
+		DownloadDirectory:      defaultDownloadDir,
+		MaxTrackerConnections:  20,
+		MaxFailedRetries:       3,
+	}
 }
