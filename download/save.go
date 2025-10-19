42975fc36993dfb4b99c41eaf099b2ea4ecfd5fc
JoelVCrasta <loejstarc@gmail.com>
2025-10-19 13:19:12 +0530

    added piece writer

--- download/save.go
+++ download/save.go
@@ -91,6 +91,60 @@ func NewPieceWriter(torrent metainfo.Torrent) (*PieceWriter, error) {
 	return pw, nil
 }
 
+func (pw *PieceWriter) WritePiece(cp *completedPiece) error {
+	pieceStart := cp.index * pw.torrent.Info.PieceLength
+	pieceLen := cp.length
+	pieceEnd := pieceStart + pieceLen
+	bufOffset := 0
+
+	if pw.torrent.IsMultiFile {
+		for _, file := range pw.torrent.Info.Files {
+			fileStart := file.Offset
+			fileEnd := fileStart + file.Length
+
+			// If piece does not overlap with file, skip
+			if pieceEnd <= fileStart || pieceStart >= fileEnd {
+				continue
+			}
+
+			// Piece overlaps with file
+			writeStart := max(pieceStart, fileStart)
+			writeEnd := min(pieceEnd, fileEnd)
+			writeLen := writeEnd - writeStart
+
+			filePath := filepath.Join(config.Config.DownloadDirectory, pw.torrent.Info.Name, file.Path)
+			f := pw.files[filePath]
+			if f == nil {
+				return fmt.Errorf("file not found for path: %s", filePath)
+			}
+
+			fileOffset := writeStart - fileStart
+			_, err := f.WriteAt(cp.buf[bufOffset:bufOffset+writeLen], int64(fileOffset))
+			if err != nil {
+				return fmt.Errorf("failed to write to file: %v", err)
+			}
+
+			bufOffset += writeLen
+			if bufOffset >= pieceLen {
+				break
+			}
+		}
+	} else {
+		filePath := filepath.Join(config.Config.DownloadDirectory, pw.torrent.Info.Name)
+		f := pw.files[filePath]
+		if f == nil {
+			return fmt.Errorf("file not found for path: %s", filePath)
+		}
+
+		_, err := f.WriteAt(cp.buf, int64(pieceStart))
+		if err != nil {
+			return fmt.Errorf("failed to write to file: %v", err)
+		}
+	}
+
+	return nil
+}
+
 func (pw *PieceWriter) CloseWriter() {
 	for _, file := range pw.files {
 		_ = file.Close()
